b7eeb5731b0625268f93c9091ebe5b4d9672e90a
AlexMunoz9 <alex.2munoz9.@gmail.com>
2025-04-08 10:51:11 -0600

    Ejercicios en clases

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,6 +1,6 @@
 #Recibir nombre y apellido
-int nombre = input("Ingrese su nombre: ")
-int apellido = input("Ingrese su apellido: ")
-nombre_completo = nombre +   apellido
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+nombre_completo = nombre  + " " + apellido 
 print(f"Nombre completo: {nombre_completo}")
 
